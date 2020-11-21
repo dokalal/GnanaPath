c394cdf19cf2e08150a1e6913f57a82be20a168e
rvchamarti <ravi.chamarti@gmail.com>
2020-11-21 18:50:13 +0000

    Add json esc val and neo4j filter

--- gnutils/replace_spl_chars.py
+++ gnutils/replace_spl_chars.py
@@ -18,3 +18,26 @@ def filter_chars(*args, **kwargs):
     if not arg_len==0:
         return args[len(args)-1]
     return "Function called with no args"
+
+
+
+
+def       gnutils_filter_neo4j_val(dtval):
+
+         dtvalstr = str(dtval);
+         dtval_s1 = dtvalstr.replace("'", "\\'");
+         dtval_s2 = dtval_s1.replace('"', '\\"');
+         ###dataval_s3 = dataval_s2.replace('\\', '\\\')
+         dtval_filtered = str(dtval_s2);
+         return dtval_filtered;
+
+
+def       gnutils_filter_json_escval(dtval):
+
+         dtvalstr = str(dtval);
+         dtval_s2 = dtvalstr.replace('"', '\\"');
+         ###dataval_s3 = dataval_s2.replace('\\', '\\\')
+         dtval_filtered = str(dtval_s2);
+         return dtval_filtered;
+
+     
